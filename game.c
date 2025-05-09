b4a4afaec7f814017cdf70445b99f244268d4b45
Hoffalo <lorenzohoff2006@gmail.com>
2025-05-09 15:30:37 +0200

    a

--- game.c
+++ game.c
@@ -1,5 +1,41 @@
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
-#include <stdlib.h>;
+
+#define MAX_NAME_LENGTH 50
+
+typedef struct {
+    char name[MAX_NAME_LENGTH];
+    int quantity;
+} Item;
+
+typedef struct {
+    Item* items;
+    int capacity;
+    int count;
+} Inventory;
+
+void InitInventory(Inventory* inv) {
+    inv->capacity = 1;
+    inv->count = 0;
+    inv->items = (Item*)malloc(sizeof(Item) * inv->capacity);
+    if (inv->items == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void expandInventory(Inventory* inv, int additional_space) {
+    int new_capacity = inv->capacity + additional_space;
+    Item* new_items = realloc(inv->items, sizeof(Item) * new_capacity);
+    if (!new_items) {
+        perror("Failed to expand inventory");
+        return;
+    }
+    inv->items = new_items;
+    inv->capacity = new_capacity;
+}
+
 
 void main() {
 
